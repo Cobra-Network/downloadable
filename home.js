e2bb60276ec05dcf529e3417aeae2b0dc05f94b9
wayzeethedev <160151058+wayzeethedev@users.noreply.github.com>
2025-03-01 01:13:56 -0600

    Update home.js

--- home.js
+++ home.js
@@ -6,8 +6,7 @@ window.onload = function() {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Genesis</title>
-    <link rel="stylesheet" href="styles.css">
-    <script defer src="scripts.js"></script>
+    <link rel="stylesheet" href="https://raw.githubusercontent.com/Cobra-Network/downloadable/refs/heads/main/styles.css">
   </head>
   <body>
     <h1>Genesis Downloadable File</h1>
